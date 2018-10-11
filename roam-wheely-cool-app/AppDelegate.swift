acb8487e8ce3616bd259ace7060b8ae42789e70d
magpali <victormagpali@hotmail.com>
2018-10-11 01:03:08 -0300

    Project initialization

---
+++ roam-wheely-cool-app/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  roam-wheely-cool-app
+//
+//  Created by victor.magpali on 10/11/18.
+//  Copyright © 2018 Victor Magpali. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let initialViewController = ViewController()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
+
